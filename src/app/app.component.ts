8e2c4d0d3c701309ec964229942302cb7b9eab0e
Juuso Valkeejärvi <juuso.valkeejarvi@enerkey.com>
2022-03-07 12:25:25 +0200

    Kendo jquery numerictextbox bug reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'kendo-numerictextbox-angular-bug';
+export class AppComponent implements AfterViewInit {
+  public ngAfterViewInit(): void {
+    const container = $('#gridContainer');
+    container.kendoGrid({
+      filterable: true,
+      dataSource: {
+        data: [
+          { value: 3 },
+          { value: 4 },
+          { value: 5 },
+          { value: 6 },
+        ],
+        schema: {
+          model: {
+            fields: {
+              value: { type: 'number' }
+            }
+          }
+        },
+      },
+      columns: [
+        { field: 'value' }
+      ],
+    })
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import '@progress/kendo-ui';
+
+import 'jquery';
+import 'expose-loader?exposes=$,jQuery!jquery';
 
 if (environment.production) {
   enableProdMode();
